5a325ec294bf0fdbdcd0fef774f956edbb845340
taraldjoh <tarald.johansen@outlook.com>
2019-02-08 19:13:41 +0100

    Security fixes

--- dist/js/main.js
+++ dist/js/main.js
@@ -25,7 +25,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItem.forEach(item => item.classList.add("show"));
+    navItem.forEach(item => item.classList.remove("show"));
 
     // Set Menu state
     showMenu = false;
