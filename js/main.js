70bb2369aa86f7a2c7e8977ca12da69620bfcc89
Tarald Johansen <41441415+taraldjoh@users.noreply.github.com>
2019-03-06 16:43:03 +0100

    added typewriter effect with js

--- js/main.js
+++ js/main.js
@@ -29,3 +29,68 @@ document.querySelector("textarea").addEventListener("keyup", e => {
   document.querySelector("#charNum").innerHTML =
     MAX_LENGTH - e.target.value.length;
 });
+
+// Type Writer Effect
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait =  parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+}
+
+// Type Method
+TypeWriter.prototype.type = function() {
+  const current = this.wordIndex % this.words.length;
+  // Get Full Text of Current Word
+  const fulltxt = this.words[current];
+
+  // Check If deleting
+  if(this.isDeleting) {
+    // Remove character
+    this.txt = fulltxt.substring(0, this.txt.length -1);
+  } else {
+    // Add character
+    this.txt = fulltxt.substring(0, this.txt.length + 1);
+  }
+
+  // Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  // Initial Type Speed 
+  let typeSpeed = 200;
+
+  if(this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  // Check if word is complete
+  if(!this.isDeleting && this.txt === fulltxt) {
+    // Make pause at end
+    typeSpeed = this.wait;
+    // Set isDeleting to True
+    this.isDeleting = true;
+  } else if(this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    // Move to next word
+    this.wordIndex++;
+    // Pause before next word
+    typeSpeed = 800;
+  }
+
+  setTimeout(() => this.type(), typeSpeed)
+}
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init Typewriter
+  new TypeWriter(txtElement, words, wait);
+}
