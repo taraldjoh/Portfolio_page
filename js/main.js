1fab71e39c1c706f39c38301236e030e6df699a6
Tarald Johansen <41441415+taraldjoh@users.noreply.github.com>
2019-04-16 02:23:00 +0200

    added arrow function and formatting

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const navItem = document.querySelectorAll(".nav-link");
 
 //Changes current class when nav item is clicked
 for (let i = 0; i < navItem.length; i++) {
-  navItem[i].addEventListener("click", function() {
+  navItem[i].addEventListener("click", () => {
     Array.from(navItem, navItem => navItem.classList.remove("current"));
     navItem[i].classList.add("current");
   });
 }
 
 // Smooth Scrolling
-$(".btn", "a").on("click", function(event) {
+$(".btn", "a").on("click", e => {
   if (this.hask !== "") {
     event.preventDefault();
     const hash = this.hash;
@@ -34,12 +34,12 @@ document.querySelector("textarea").addEventListener("keyup", e => {
 const TypeWriter = function(txtElement, words, wait = 3000) {
   this.txtElement = txtElement;
   this.words = words;
-  this.txt = '';
+  this.txt = "";
   this.wordIndex = 0;
-  this.wait =  parseInt(wait, 10);
+  this.wait = parseInt(wait, 10);
   this.type();
   this.isDeleting = false;
-}
+};
 
 // Type Method
 TypeWriter.prototype.type = function() {
@@ -48,9 +48,9 @@ TypeWriter.prototype.type = function() {
   const fulltxt = this.words[current];
 
   // Check If deleting
-  if(this.isDeleting) {
+  if (this.isDeleting) {
     // Remove character
-    this.txt = fulltxt.substring(0, this.txt.length -1);
+    this.txt = fulltxt.substring(0, this.txt.length - 1);
   } else {
     // Add character
     this.txt = fulltxt.substring(0, this.txt.length + 1);
@@ -59,20 +59,20 @@ TypeWriter.prototype.type = function() {
   // Insert txt into element
   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-  // Initial Type Speed 
+  // Initial Type Speed
   let typeSpeed = 200;
 
-  if(this.isDeleting) {
+  if (this.isDeleting) {
     typeSpeed /= 2;
   }
 
   // Check if word is complete
-  if(!this.isDeleting && this.txt === fulltxt) {
+  if (!this.isDeleting && this.txt === fulltxt) {
     // Make pause at end
     typeSpeed = this.wait;
     // Set isDeleting to True
     this.isDeleting = true;
-  } else if(this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
     // Move to next word
     this.wordIndex++;
@@ -80,17 +80,17 @@ TypeWriter.prototype.type = function() {
     typeSpeed = 800;
   }
 
-  setTimeout(() => this.type(), typeSpeed)
-}
+  setTimeout(() => this.type(), typeSpeed);
+};
 
 // Init On DOM Load
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 // Init App
 function init() {
   const txtElement = document.querySelector(".txt-type");
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
   // Init Typewriter
   new TypeWriter(txtElement, words, wait);
 }
